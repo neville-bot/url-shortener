c50e8244dc85806f2ba5ab81ce6167dd9d6cf72f
John Nevin <jnevin17@gmail.com>
2022-03-09 16:20:04 -0600

    Mongo DB config finished

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const redirectRouter = require("./server/API-routes/redirect.js")
 const app = express()
 const PORT = process.env.PORT || 5000
 
+// DB connection
+const connection = require("./server/config/db.config.js")
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully")
+})
+connection.on("error", (err) => {
+  console.log("MongoDB connection error:", err)
+})
+
 // middleware
 app.use(express.json()) // parses incoming request with JSON payloads (based on body-parser)
 app.use(express.urlencoded({ extended: false }))
-app.use("/", shortenerRouter)
-// app.use("/testAPI", redirectRouter)
+app.use("/", redirectRouter)
+app.use("/api/url", shortenerRouter)
 
 // error handling endware
 app.use((err, req, res, next) => {
