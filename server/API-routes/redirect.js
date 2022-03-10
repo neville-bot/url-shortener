e5b50b756e6a1bc2dc61383b0c9e3e4a844329f1
John Nevin <jnevin17@gmail.com>
2022-03-10 11:07:30 -0600

    update to redirect route

--- server/API-routes/redirect.js
+++ server/API-routes/redirect.js
@@ -2,16 +2,17 @@ const express = require("express")
 const Url = require("../models/Urlmodel")
 const router = express.Router()
 
-router.get("/:shortUrl", async (req, res) => {
-  const shortUrl = req.params.shortUrl
+router.get("/", async (req, res) => {
+  const ogUrl = req.params.url
+
   try {
-    const url = await Url.findOne({ shortUrl: shortUrl })
+    const url = await Url.findOne({ originalUrl: ogUrl })
     if (url) {
-      res.redirect(url.originalUrl)
+      // res.redirect(url.shortUrl)
     } else {
       res.status(404).json({ error: "Invalid URL" })
     }
-  } catch {
+  } catch (err) {
     console.error(err)
     res.status(500).json({ error: err.message })
   }
